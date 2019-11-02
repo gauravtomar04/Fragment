872b77c81bf14e5cf1ea844b04e16b7205caebcb
Gaurav Tomar <gauravtomar9210@gmail.com>
2019-11-02 10:24:45 +0530

    Fragment

---
+++ app/src/main/java/gauravtomar/fragment/Frg2.java
@@ -0,0 +1,34 @@
+package gauravtomar.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class Frg2 extends Fragment {
+
+    View view;
+    Button secondButton;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+// Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.activity_fragment, container, false);
+// get the reference of Button
+        secondButton = (Button) view.findViewById(R.id.secondButton);
+// perform setOnClickListener on second Button
+        secondButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+// display a message by using a Toast
+                Toast.makeText(getActivity(), "Second Fragment", Toast.LENGTH_LONG).show();
+            }
+        });
+        return view;
+    }
+}
